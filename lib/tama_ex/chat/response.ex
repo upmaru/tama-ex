6208cce48e64a92ee06526e065cf5b7c3a54d44a
Zack Siri <me@zacksiri.dev>
2025-09-01 16:46:01 +0700

    Add Chat response resource

---
+++ lib/tama_ex/chat/response.ex
@@ -0,0 +1,46 @@
+defmodule TamaEx.Chat.Response do
+  def create(client, body, options \\ []) do
+    with {:ok, validated_client} <- TamaEx.validate_client(client, ["api"]) do
+      path = "/chat/completions"
+      timeout = Keyword.get(options, :timeout) || 300_000
+      headers = Keyword.get(options, :headers) || []
+
+      stream_handler =
+        if body["stream"] do
+          Keyword.get(options, :stream)
+        end
+
+      if body["stream"] && is_nil(stream_handler) do
+        raise """
+        Stream handler is required when streaming is true pass a stream handler into options
+
+        Example:
+
+        fn {:data, data}, context ->
+          Phoenix.PubSub.broadcast(YourApp.PubSub, "message:12", {:chunk, data})
+
+          {:cont, context}
+        end
+        """
+      end
+
+      request =
+        Req.merge(validated_client,
+          method: :post,
+          url: path,
+          json: body,
+          receive_timeout: timeout,
+          headers: headers
+        )
+
+      request =
+        if stream_handler do
+          Req.merge(request, into: stream_handler)
+        else
+          request
+        end
+
+      Req.request!(request)
+    end
+  end
+end
